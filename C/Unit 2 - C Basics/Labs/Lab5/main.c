e0b3149f249cbac7cca14476ee943b49a34ad903
MohamedAshry <mga.ashryggmail.com>
2020-11-14 22:41:50 +0100

    solve the lab

--- C/Unit 2 - C Basics/Labs/Lab5/main.c
+++ C/Unit 2 - C Basics/Labs/Lab5/main.c
@@ -5,4 +5,17 @@
  *      Author: MohamedAshry
  */
 
+/* description:
+ * calculate the sum of numbers between 1 to 99.
+ */
 
+#include <stdio.h>
+
+void main ()
+{
+	int number, sum=0;
+	for (number = 0; number<=99; number++)
+		sum += number;
+	printf ("The summation of the numbers between 1 to 99 is: %d",sum);
+	getchar();
+}
