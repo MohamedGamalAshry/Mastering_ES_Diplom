c024d35f1976411e3a781b108c9d8148ee92076c
MohamedAshry <mga.ashryggmail.com>
2020-11-14 22:25:46 +0100

    finish coding

--- C/Unit 2 - C Basics/Labs/Lab4/main.c
+++ C/Unit 2 - C Basics/Labs/Lab4/main.c
@@ -5,4 +5,22 @@
  *      Author: MohamedAshry
  */
 
+/*
+ * Description: Calculate the minimum of two numbers
+ */
 
+#include <stdio.h>
+
+void main ()
+{
+	int a, b ;								// declaration of varialbles
+
+	printf("Enter the two numbers: \r\n");	// Ask user to enter the two numbers
+	scanf("%d %d",&a ,&b);					// scan the numbers from the user
+	fflush(stdin);
+	fflush(stdout);
+
+	printf("the minimum number is: %d", (a>b) ?b:a);	// print the minimum number through in-line condition
+	getchar();
+
+}	// end of main
