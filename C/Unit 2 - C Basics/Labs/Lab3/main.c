47701f557bc002e922889b67710ab1b223312945
MohamedAshry <mga.ashryggmail.com>
2020-11-14 21:55:13 +0100

    add main.c and initial solve the lab

---
+++ C/Unit 2 - C Basics/Labs/Lab3/main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 14, 2020
+ *      Author: MohamedAshry
+ */
+
+/*
+ * Description:
+ * the program is used to compare three numbers entered by the user and find
+ * the largest value of the the three numbers.
+ *
+ */
+
+#include <stdio.h>
+
+void main ()
+{
+	// declaration and initialization:
+	int first, second, third ;
+
+	printf ("Enter the three numbers: \r\n") ;
+	scanf ("%d %d %d", &first, &second, &third);
+
+	if (first > second)
+	{
+		if (first > third)
+			printf("The largest number is: %d", first);
+		else
+			printf("The largest number is: %d", third);
+	}
+	else
+	{
+		if (second > third)
+			printf("The largest number is: %d", second);
+		else
+			printf("The largest number is: %d", third);
+
+	}
+}
