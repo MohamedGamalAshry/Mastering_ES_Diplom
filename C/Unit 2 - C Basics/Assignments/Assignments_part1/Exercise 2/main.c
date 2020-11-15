e438e7d890a2ac586ea76097cb64158dfcbb5483
MohamedAshry <mga.ashryggmail.com>
2020-11-15 01:28:17 +0100

    solve Ex.2 with comments

--- C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 2/main.c
+++ C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 2/main.c
@@ -3,6 +3,17 @@
  *
  *  Created on: Nov 15, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: C Program to Print a Integer Entered by a User
  */
 
+#include <stdio.h>
 
+int main()
+{
+    int number;										// declaring an integer variable
+    printf("Enter an integer: ");  					// ask the user to enter the number
+    scanf("%d",&number);   							// scan the integer and store it
+    printf("\r\nYou have entered: %d",number);		// print the entered number
+    return 0;
+}	// end of main
