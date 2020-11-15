c4cdf62aac40b70b8d46a736906498722152d97c
MohamedAshry <mga.ashryggmail.com>
2020-11-15 01:02:47 +0100

    solve first Exercise in assignments_part1

---
+++ C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 15, 2020
+ *      Author: MohamedAshry
+ *
+ *      Description: Program to Print a Sentence "C Programming"
+ */
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("C Programming");
+    return 0;
+}
