a9d3370728820d79e4dc68eadc4c2e0c51b01694
MohamedAshry <mga.ashryggmail.com>
2020-11-15 12:12:15 +0100

    write the main function

--- C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 5/main.c
+++ C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 5/main.c
@@ -3,6 +3,20 @@
  *
  *  Created on: Nov 15, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: This program finds the ASCII value of the entered charachter
+ *
  */
 
 
+#include <stdio.h>
+int main()
+{
+	char CH;
+	printf("Enter a character: ");				// Ask user to enter the character
+	scanf("%c",&CH);        					// store the character from user
+	fflush(stdin);	fflush(stdout);
+
+	printf("ASCII value of %c = %d", CH, CH);	// print the character as an integer value
+	return 0;
+}		// end of main
