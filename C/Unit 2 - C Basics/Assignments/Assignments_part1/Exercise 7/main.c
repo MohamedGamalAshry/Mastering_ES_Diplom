b233e0210fbb4dec550ed95b7d10326cb88d8f4f
MohamedAshry <mga.ashryggmail.com>
2020-11-15 12:45:09 +0100

    fully write the main function

--- C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 7/main.c
+++ C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 7/main.c
@@ -3,6 +3,32 @@
  *
  *  Created on: Nov 15, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: Write C Code to Swap Two Numbers without Temp variable.
  */
 
+#include<stdio.h>
 
+#include <stdio.h>
+
+int main()
+{
+	float First, Second;							// declaration of variables
+
+	printf("Enter the first value: ");				// ask user to enter the first value
+	scanf("%f", &First);							// store the first value
+	fflush(stdin);	fflush(stdout);
+
+	printf("\r\nEnter the second value: ");			// ask user to enter the second value
+	scanf("%f", &Second);							// store the second value
+	fflush(stdin);	fflush(stdout);
+
+	printf("Before swap First=%d Second=%d", First, Second);
+	fflush(stdout);
+	First  = First + Second;
+	Second = First - Second;
+	First  = First - Second;
+
+	printf("\nAfter swap First=%d Second=%d", First, Second);
+	return 0;
+}
