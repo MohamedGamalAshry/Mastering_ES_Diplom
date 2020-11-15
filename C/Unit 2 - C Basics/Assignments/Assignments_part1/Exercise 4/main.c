c7268ddd45ddf7e282c110f29f7f408041ed22ca
MohamedAshry <mga.ashryggmail.com>
2020-11-15 11:51:55 +0100

    write the main function

---
+++ C/Unit 2 - C Basics/Assignments/Assignments_part1/Exercise 4/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 15, 2020
+ *      Author: MohamedAshry
+ *
+ *      Description: Program to Multiply two Floating Point Numbers and display the result
+ */
+
+
+#include <stdio.h>
+int main( )
+{
+    //declaration of variables
+	float number_1, number_2, Result;
+
+    printf("Enter two numbers: ");					// ask user to enter the numbers
+    scanf("%f %f",&number_1,&number_2);        		// Store the two floating point numbers entered by use
+    fflush(stdin) ; fflush(stdout);
+
+    Result = number_1*number_2;  					// calculate the result of  multiplication
+    printf("Product: %f",Result);
+    return 0;
+}
