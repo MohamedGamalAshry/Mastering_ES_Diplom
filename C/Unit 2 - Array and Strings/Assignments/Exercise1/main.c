431497820aebb0a13d68fa65017c4984870710e4
MohamedAshry <mga.ashryggmail.com>
2020-11-20 04:17:44 +0100

    write the main function of the program

--- C/Unit 2 - Array and Strings/Assignments/Exercise1/main.c
+++ C/Unit 2 - Array and Strings/Assignments/Exercise1/main.c
@@ -3,6 +3,29 @@
  *
  *  Created on: Nov 20, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: Write a C Program to find the sum of 2 matrix 2*2 where the elements of
+ *      			 two matrices are entered by the user
  */
 
+#include <stdio.h>
 
+int main ()
+{
+	float matrix1 [2][2];
+	float matrix2 [2][2];
+	int i , j;
+
+	printf ("Enter the elements of Matrix No.1: \n\r") ;
+	fflush (stdin);	fflush(stdout);
+
+	for ( i=0 ; i<2 ; i++)
+	{
+		for ( j=0 ; j<2 ; j++)
+		{
+			printf ("Enter the element of No. %d%d: \n\r",i+1,j+1);
+			scanf ("%f", &matrix1[i][j]) ;
+		}
+	}
+	return 0;
+}
