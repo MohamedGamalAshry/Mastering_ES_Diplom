d737c133b5d2e50fb1dcb86be391aee136438290
MohamedAshry <mga.ashryggmail.com>
2020-11-20 02:02:45 +0100

    Finish Lab1 in Array and Strings

---
+++ C/Unit 2 - Array and Strings/Labs/Lab1/main.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 18, 2020
+ *      Author: MohamedAshry
+ *
+ *      Description: Store and print the degree of 10 students
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+	float degrees [10];		// declaring an array of 10 students
+	int count;
+
+	for(count = 0 ; count <10 ; count++ )	// looping to get and store the degrees
+	{
+		printf ("Enter the degree of student No.%d\r\n",count+1);
+		scanf ("%f", &degrees[count]);
+		fflush(stdin) ; fflush(stdout);
+
+	}
+
+	for(count = 0 ; count <10 ; count++ )	// looping to print the students degrees
+	{
+		printf ("The degree of Student No. %d is: %f\r\n", count+1,degrees[count]);
+	}
+	return 0;
+
+}
