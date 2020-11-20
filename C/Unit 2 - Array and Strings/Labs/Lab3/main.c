13025b1b165dc265547e59ada15483d9854b9b84
MohamedAshry <mga.ashryggmail.com>
2020-11-20 02:49:55 +0100

    add comments to the code

--- C/Unit 2 - Array and Strings/Labs/Lab3/main.c
+++ C/Unit 2 - Array and Strings/Labs/Lab3/main.c
@@ -11,8 +11,8 @@
 
 int main()
 {
-	float matrix [3][3] ;
-	float transpose [3][3];
+	float matrix [3][3];		// declaring the matrix array
+	float transpose [3][3];		// declaring the transpose matrix array
 	int i,j;
 
 	//reading the matrix from the user:
@@ -20,42 +20,45 @@ int main()
 	{
 		for (j=0 ; j<3 ; j++)
 		{
-			printf("Enter the matrix element of (%d,%d)\r\n", i,j );
+			printf("Enter the matrix element of (%d,%d)\r\n", i,j );	// read and scan the matrix elements from user
 			fflush(stdin) ; fflush(stdout);
 			scanf("%f", &matrix[i][j]);
 		}
 	}
+
 	// printing the matrix
 	printf("The matrix is:\r\n");
 	for (i=0 ; i<3 ; i++)
 	{
 		for (j=0 ; j<3 ; j++)
 		{
-			printf("%.2f ", matrix[i][j]);
+			printf("%.2f ", matrix[i][j]);		// print the matix elements each iteration
 			fflush(stdin) ; fflush(stdout);
 
-		}
-		printf("\r\n");
-	}
+		}	//end of for loop
+		printf("\r\n");							// make new line after each matrix row
+	}	//end of for loop
 
 	//convert the matrix to its transpose:
 	for (i=0 ; i<3 ; i++)
 	{
 		for (j=0 ; j<3 ; j++)
 		{
-			transpose [i][j] = matrix [j][i] ;
-		}
-	}
+			transpose [i][j] = matrix [j][i] ;	// convert the matrix elements to the transpose
+		}	//end of for loop
+	}	//end of for loop
+
 	// printing the matrix
 	printf("The transpose of the matrix is:\r\n");
 	for (i=0 ; i<3 ; i++)
 	{
 		for (j=0 ; j<3 ; j++)
 		{
-			printf("%.2f ", transpose[i][j]);
+			printf("%.2f ", transpose[i][j]);	// print the tranpose matrix
 
-		}
+		}	//end of for loop
 		printf("\r\n");
-	}
+	}	//end of for loop
 
-}
+	return 0;
+}	// end of main
