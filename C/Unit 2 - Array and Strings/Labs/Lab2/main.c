57361dddc1430a038e3c4c24e9173adb5dc3601d
MohamedAshry <mga.ashryggmail.com>
2020-11-20 02:10:46 +0100

    write the main function of the program

--- C/Unit 2 - Array and Strings/Labs/Lab2/main.c
+++ C/Unit 2 - Array and Strings/Labs/Lab2/main.c
@@ -3,6 +3,23 @@
  *
  *  Created on: Nov 20, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: the program calculate the polynomial value for a set of inputs
  */
 
 
+#include <stdio.h>
+
+void main ()
+{
+	float x [5]={5,16,22,3.5,15};
+	float y;
+	int i ;
+
+	for (i=0; i<5 ; i++)
+	{
+		y = 5*x*x+3*x+2;
+		printf("y(%f)=%f\r\n",x,y);
+	}
+
+}
