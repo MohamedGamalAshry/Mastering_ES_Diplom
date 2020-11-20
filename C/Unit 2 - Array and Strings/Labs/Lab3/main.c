a7c68463c3169d419dedc1931e52a8eeb64013bb
MohamedAshry <mga.ashryggmail.com>
2020-11-20 02:42:31 +0100

    read and print the original matrix

--- C/Unit 2 - Array and Strings/Labs/Lab3/main.c
+++ C/Unit 2 - Array and Strings/Labs/Lab3/main.c
@@ -3,6 +3,37 @@
  *
  *  Created on: Nov 20, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: calculate and print the transpose of 3*3 matrix
  */
 
+#include<stdio.h>
 
+int main()
+{
+	float matrix [3][3] ;
+	float transpose [3][3];
+	int i,j;
+
+	//reading the matrix from the user:
+	for (i=0 ; i<3 ; i++)
+	{
+		for (j=0 ; j<3 ; j++)
+		{
+			printf("Enter the matrix element of (%d,%d)\r\n", i,j );
+			fflush(stdin) ; fflush(stdout);
+			scanf("%f", &matrix[i][j]);
+		}
+	}
+	// printing the matrix
+	printf("The matrix is:\r\n");
+	for (i=0 ; i<3 ; i++)
+	{
+		for (j=0 ; j<3 ; j++)
+		{
+			printf("%.2f ", matrix[i][j]);
+
+		}
+		printf("\r\n");
+	}
+}
