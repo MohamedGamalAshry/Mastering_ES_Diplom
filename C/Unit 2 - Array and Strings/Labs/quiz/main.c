2f28a4191fad69f3bdcd27cb28a62d28a4f478af
MohamedAshry <mga.ashryggmail.com>
2020-11-20 03:19:35 +0100

    write the main function of the Quiz

--- C/Unit 2 - Array and Strings/Labs/quiz/main.c
+++ C/Unit 2 - Array and Strings/Labs/quiz/main.c
@@ -3,6 +3,38 @@
  *
  *  Created on: Nov 20, 2020
  *      Author: MohamedAshry
+ *
+ *      Description: Define 1D array of 1000 element and fill it with a random values using rand() function
+ *      			 between 1 and 1000, then search and counts the number of values that satisfy the following condition:
+ *      			 	the previous and the next values is lower than the center value.
  */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int x [1000];
+	int i , count = 0;
+	int found [];
+
+
+	for (i=0 ; i<1000 ; i++)
+	{
+		x [i] = rand() % 1000;
+		//printf("%d \r\n", x[i])
+	}
+
+	for (i=1 ; i<999 ; i++)
+	{
+		if (x[i] > x[i-1] && x[i] > x[i+1] )
+		{
+			printf(" The array element No. %d fulfill the condition", x[i]);
+			count+= 1;
+		}
+	}
+	printf(" The total array elements that fulfill the condition is:", count);
+
+}
 
