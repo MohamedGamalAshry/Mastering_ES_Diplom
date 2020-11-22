644d8070662e34e3ff60e2e3003d01d484b3ec4c
MohamedAshry <mga.ashryggmail.com>
2020-11-22 01:54:53 +0100

    write the full function with comments

---
+++ C/Unit 2 - Array and Strings/Assignments/Strings/Assignment_3/main.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: MohamedAshry
+ *
+ *      Description: the program is used to reverse the entered string.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+	char string[100] ;				// declaring an array to store the entered string from the user
+	char reverse[100];				// declaring an array to store the reversed string
+	int length ;
+
+	printf("Enter a string: ");		// ask user to enter the string
+	fflush(stdin) ;
+	fflush(stdout) ;
+	scanf ("%[^\n]%*c",string);
+
+	length = strlen(string);
+	for (int i=0; i<length ; i++)
+	{
+		reverse [i] = string[length-i-1] ;	// reverse the string
+	}
+
+	reverse[length]= 0;				// end the string with null character
+	// print the original and reversed string:
+	printf("\r\nThe entered string is: %s\r\nThe reversed string is:  %s",string, reverse);
+
+	return 0;
+}
+
