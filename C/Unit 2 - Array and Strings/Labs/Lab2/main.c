5d188109be54a15f84fd46511435bf3af9cc923b
MohamedAshry <mga.ashryggmail.com>
2020-11-22 12:47:55 +0100

    add printf to interface with the user

--- C/Unit 2 - Array and Strings/Labs/Lab2/main.c
+++ C/Unit 2 - Array and Strings/Labs/Lab2/main.c
@@ -12,11 +12,13 @@
 
 int main ()
 {
-	float x [5]={5,16,22,3.5,15};			// declaring and initialization of array
+	float x[5]={5,16,22,3.5,15};			// declaring and initialization of array
 	float y[5];								// declaration of the result variable
 	int i ;									// declaring the looping variable
 
 	// looping to evaluate the polynomial expression
+	printf("Evaluation of the polynomial y=5*x^2+3*x+2, for x = {5,16,22,3.5,15} is:\r\n");
+
 	for (i=0; i<5 ; i++)
 	{
 		y[i] = 5*x[i]*x[i]+3*x[i]+2;			// evaluate the function
