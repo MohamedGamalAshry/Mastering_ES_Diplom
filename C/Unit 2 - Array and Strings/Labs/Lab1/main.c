b567f9776f6d86573640bab678258ee61828123c
MohamedAshry <mga.ashryggmail.com>
2020-11-22 12:34:24 +0100

    recompile and build the program

--- C/Unit 2 - Array and Strings/Labs/Lab1/main.c
+++ C/Unit 2 - Array and Strings/Labs/Lab1/main.c
@@ -12,19 +12,20 @@
 
 int main ()
 {
-	float degrees [10];		// declaring an array of 10 students
+	float degrees [10];		 				// declaring an array of 10 students
 	int count;
 
 	for(count = 0 ; count <10 ; count++ )	// looping to get and store the degrees
 	{
 		printf ("Enter the degree of student No.%d\r\n",count+1);
 		fflush(stdin) ; fflush(stdout);
-		scanf ("%.2f", &degrees[count]);
+		scanf ("%f", &degrees[count]);
 	}
 
 	for(count = 0 ; count <10 ; count++ )	// looping to print the students degrees
 	{
 		printf ("The degree of Student No. %d is: %f\r\n", count+1,degrees[count]);
+
 	}
 	return 0;
 
