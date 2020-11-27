93d82f854e3673093b74abeb636c44d7458231b7
MohamedAshry <mga.ashryggmail.com>
2020-11-27 15:16:49 +0100

    write the full main function

--- C/Unit 2 - Functions/Labs/Lab 4/main.c
+++ C/Unit 2 - Functions/Labs/Lab 4/main.c
@@ -12,6 +12,39 @@
  *      			 and so on
  */
 
+# include <stdio.h>
 
+// Function declaration (Prototypes):
 
+void createPattern (int final_number);
+
+// Main Function
+
+int main ()
+{
+	int final_number;
+	// Read and store the pattern limit from the user
+	printf ("Enter the final number that represent the Pattern limit: ");
+	fflush(stdin);		fflush(stdout);
+	scanf ("%d", &final_number);
+
+	printf ("\r\nThe Pattern:\r\n ");
+	createPattern (final_number);
+
+	return 0;
+}		// end of main
+
+/*  ------------------------------------------------------- */
+
+// Function definition (Function body):
+
+void createPattern (int final_number)
+{
+	for (int i=0 ; i<=final_number ; i++)
+		{
+			for (int j = i; j<=final_number ; j++)
+				printf("%d ",j);
+			printf("\r\n");
+		}	//end of 2nd for
+}
 
