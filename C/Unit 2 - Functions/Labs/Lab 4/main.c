e207d29a3716e08770c1a15bf275bc78489de9e3
MohamedAshry <mga.ashryggmail.com>
2020-11-27 15:05:02 +0100

    add main.c

---
+++ C/Unit 2 - Functions/Labs/Lab 4/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 27, 2020
+ *      Author: MohamedAshry
+ *
+ *      Description: create a pattern of numbers and print it as each line contain less number than
+ *      			 the above line for example:
+ *      			 0 1 2 3 4 5 6 7 8 9
+ *      			 1 2 3 4 5 6 7 8 9
+ *      			 2 3 4 5 6 7 8 9
+ *      			 and so on
+ */
+
+
+
+
