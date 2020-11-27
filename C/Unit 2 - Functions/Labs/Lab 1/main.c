50e953807c8265e2ee10929cfbc37926d475aa27
MohamedAshry <mga.ashryggmail.com>
2020-11-27 11:28:05 +0100

    add main.c to the project

---
+++ C/Unit 2 - Functions/Labs/Lab 1/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 27, 2020
+ *      Author: MohamedAshry
+ *
+ *      Description: C program using functions to calculate the factorial of a specific number.
+ *      			The number will be entered from the user and the result of its factorial will
+ *      			be displayed on the screen.
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+
+	return 0;
+}
+
