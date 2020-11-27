28d0944010db40cc91c82b57c7ddee6378d1d6cc
MohamedAshry <mga.ashryggmail.com>
2020-11-27 14:28:54 +0100

    solving the bug in if condition by comparing the pointer value

--- C/Unit 2 - Functions/Labs/Lab 3/main.c
+++ C/Unit 2 - Functions/Labs/Lab 3/main.c
@@ -11,42 +11,45 @@
 #include <stdio.h>
 
 // Declaration of Functions:
-int findName(char names[10], int length, char name[]);
+int findName(char names[][10], int length, char name[]);
 
 int main ()
 {
-	char names [10] = {"Mohamed","Ahmed","Ali","Belal","Adham","Hamza"} ;
+	char names [][10] = {"Mohamed","Ahmed","Ali","Belal","Adham","Hamza"} ;
 	char name [20] ;
 	int length;
+
 	printf("Enter your name to log-in:\r\n") ;
 	fflush(stdin);		fflush(stdout);
-	scanf("%[^\n]%*c",&name);
+	scanf("%[^\n]%*c", &name);
 
-	length = sizeof(names) / sizeof(names[0]);
+	length = sizeof(names)/ sizeof(names[0]);
 
 	if(findName(names, length, name)==1)
-		printf ("Welcome");
+		{
+			printf ("Welcome to Embedded System");
+			break ;
+		}
 	else
-		printf ("Good bye");
+		printf ("Your name is not on the list, check it again");
 
 	return 0;
 }
 
 // Function Definition
 
-int findName(char names[10], int length, char name[])
+int findName(char names[][10], int length, char name[])
 {
 	int i ;
+	int found = 0 ;
 
 	for (i=0 ; i<length ; i++)
 	{
-		if (names[i]==name)
-			{
-				return 1 ;
-				break;
-			}
-		else
-			return 0;
-	}
+		printf("%s\r\n", names[i]);
+		printf("%s\r\n", name);
 
+		if (*names[i]==*name)
+			found = 1;
+	}
+	return found;
 }
