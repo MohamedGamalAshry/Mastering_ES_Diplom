e120ab72d1afccef8e42a748ccea67e9f1126efd
MohamedAshry <mga.ashryggmail.com>
2020-11-27 14:37:33 +0100

    add more comments

--- C/Unit 2 - Functions/Labs/Lab 3/main.c
+++ C/Unit 2 - Functions/Labs/Lab 3/main.c
@@ -15,17 +15,19 @@ int findName(char names[][10], int length, char name[]);
 
 int main ()
 {
+	// Declarng the required variables
 	char names [][10] = {"Mohamed","Ahmed","Ali","Belal","Adham","Hamza"} ;
 	char name [20] ;
-	int length;
+	int length;			// declare variable to calculate the array elements
 
+	// scan and store the name to be searched from user
 	printf("Enter your name to log-in:\r\n") ;
 	fflush(stdin);		fflush(stdout);
 	scanf("%[^\n]%*c", &name);
 
-	length = sizeof(names)/ sizeof(names[0]);
+	length = sizeof(names)/ sizeof(names[0]);	// calculate the array elements/length
 
-	if(findName(names, length, name)==1)
+	if(findName(names, length, name)==1)		// check if user name is on the list
 		printf ("Welcome to Embedded System Mr. %s", name);
 	else
 		printf ("Your name is not on the list Mr. %s, check it again", name);
@@ -40,13 +42,13 @@ int findName(char names[][10], int length, char name[])
 	int i ;
 	int found = 0 ;
 
-	for (i=0 ; i<length ; i++)
+	for (i=0 ; i<length ; i++)			// Looping through array to check each name
 	{
 		//printf("%s\r\n", names[i]);	//for debugging
 		//printf("%s\r\n", name);		//for debugging
 
-		if (*names[i]==*name)
+		if (*names[i]==*name)			// check if user name is on the list
 			found = 1;
 	}
-	return found;
+	return found;						// return found to describe if name is found or not
 }
